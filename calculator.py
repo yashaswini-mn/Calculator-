6827aa45c627cc4e8baeb6c8022fb90956d20cff
Yashaswini M N <yashaswnimn26@gmail.com>
2025-02-13 22:40:00 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,49 +1,51 @@
 class Calculator:
     
-    def addition(x,y):
+    def addition(self, x, y):
         if(x<500 and y<500):
-            print("It's easy bro can't you even do that, let it be I'll do it for you")
+            print("It's easy bro can't you even do that? Let it be, I'll do it for you")
             c=x+y
             print(f"The sum is {c}")
         elif(x>500 and y>500):
-            print("Well, this looks complicated!, Let me do this for you")
+            print("Well, this looks complicated! Let me do this for you")
             c=x+y
             print(f"The sum is {c}")
         else:
             print("Broo, are you out of your mind additon can be done only for numbers")
             
-    def subtraction(x,y):
+    def subtraction(self, x, y):
         if(x<500 and y<500):
-            print("It's easy bro can't you even do that, let it be I'll do it for you")
+            print("It's easy bro can't you even do that? Let it be, I'll do it for you")
             c=x-y
             print(f"The difference is {c}")
         elif(x>500 and y>500):
-            print("Well, this looks complicated!, Let me do this for you")
+            print("Well, this looks complicated! Let me do this for you")
             c=x-y
             print(f"The difference is {c}")
         else:
             print("Broo, are you out of your mind subtraction can be done only for numbers")
         
         
-    def multiplication(x,y):
+    def multiplication(self, x, y):
         if(x<500 and y<500):
-            print("It's easy bro can't you even do that, let it be I'll do it for you")
+            print("It's easy bro can't you even do that? Let it be, I'll do it for you")
             c=x*y
             print(f"The product is {c}")
         elif(x>500 and y>500):
-            print("Well, this looks complicated!, Let me do this for you")
+            print("Well, this looks complicated! Let me do this for you")
             c=x*y
             print(f"The product is {c}")
         else:
             print("Broo, are you out of your mind multiplication can be done only for numbers")
             
-    def division(x,y):
+    def division(self, x, y):
+        if(y==0):
+            print("Brooo..!, you can't divide by zero")
         if(x<500 and y<500):
-            print("It's easy bro can't you even do that, let it be I'll do it for you")
+            print("It's easy bro, can't you even do that? Let it be, I'll do it for you")
             c=x/y
             print(f"The divison is {c}")
         elif(x>500 and y>500):
-            print("Well, this looks complicated!, Let me do this for you")
+            print("Well, this looks complicated! Let me do this for you")
             c=x/y
             print(f"The divison is {c}")
         else:
@@ -63,20 +65,20 @@ while True:
         
     choice = int(input("Enter your choice babe's : "))
     if(choice==1):
-        n=int(input("Enter the first number"))
-        m=int(input("Enter the second number"))
+        n=int(input("Enter the first number : "))
+        m=int(input("Enter the second number : "))
         c.addition(n, m)
     elif(choice==2):
-        n=int(input("Enter the first number"))
-        m=int(input("Enter the second number"))
+        n=int(input("Enter the first number : "))
+        m=int(input("Enter the second number : "))
         c.subtraction(n, m)
     elif(choice==3):
-        n=int(input("Enter the first number"))
-        m=int(input("Enter the second number"))
+        n=int(input("Enter the first number : "))
+        m=int(input("Enter the second number : "))
         c.multiplication(n, m)
     elif(choice==4):
-        n=int(input("Enter the first number"))
-        m=int(input("Enter the second number"))
+        n=int(input("Enter the first number : "))
+        m=int(input("Enter the second number : "))
         c.division(n, m)
     elif(choice==5):
         break
